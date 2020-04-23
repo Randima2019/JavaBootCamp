6c0dc29002d76e74067012cfbc48b79e6ad1109c
Randima2019 <57174597+Randima2019@users.noreply.github.com>
2020-04-23 12:02:10 +0530

    Crud Operation Using Php
    Crud Operation Using Php

---
+++ Crud Using Php/Crudphp/server.php
@@ -0,0 +1,51 @@
+<?php
+
+    session_start();
+
+    $name ="";
+    $address ="";
+    $id = 0;
+    $edit_state =false;
+
+
+    $db = mysqli_connect('localhost', 'root', '', 'crud');
+
+    if(isset($_POST['save'])){
+        $name = $_POST['name'];
+        $address = $_POST['address'];
+
+        $query = "INSERT INTO info (name,address) VALUES('$name', '$address')";
+        mysqli_query($db, $query);
+        $_SESSION['msg'] = "Successfully Inserted ";
+        header ('location: index.php');
+    }
+
+
+//update
+if(isset($_POST['update'])){
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $address =$_POST['address'];
+    
+
+    mysqli_query($db, "UPDATE info SET name ='$name' , address='$address' WHERE id = $id " );
+    $_SESSION['msg'] ="Address updated";
+    header ('location: index.php');
+}
+
+//delete
+
+if(isset($_GET['del'])){
+    $id = $_GET['del'];
+    mysqli_query($db , "DELETE FROM info WHERE id= $id");
+    $_SESSION['msg'] ="Address Deleted";
+    header ('location: index.php');
+}
+
+
+//retrieve recodes
+
+$results = mysqli_query($db, "SELECT * FROM info");
+
+
+?>
---
+++ Crud Using Php/DB/crud.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 30, 2020 at 02:32 PM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `info`
+--
+
+DROP TABLE IF EXISTS `info`;
+CREATE TABLE IF NOT EXISTS `info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `info`
+--
+
+INSERT INTO `info` (`id`, `name`, `address`) VALUES
+(19, 'pasindu', 'Gampaha'),
+(2, 'Randima', 'tangalle'),
+(18, 'Krishan', 'Matara'),
+(16, 'Dilshan', 'galle'),
+(17, 'Shan', 'Colombo');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
